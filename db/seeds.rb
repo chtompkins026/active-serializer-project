709b71497d7d3469193ec694c839180bfbe18ebc
chtompkins026 <chtompkins026@gmail.com>
2019-02-26 11:33:41 -0500

    add seed data for Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,13 @@ require 'faker'
   Team.destroy_all
   Player.destroy_all
 
+  user1 = User.create!({name: 'Chris', password:'password', email:'chtompkins026@gmail.com'})
+  user2 = User.create!({name: 'Jason Tompkins', password:'password1', email:'chtompkins027@gmail.com'})
+  user3 = User.create!({name: 'Jason Wang', password:'password1', email:'chtompkins0299@gmail.com'})
+  user4 = User.create!({name: 'Jason Lazzaros', password:'password1', email:'chtompkins028@gmail.com'})
 
 10.times do
-  team = Team.create!({name:Faker::Name.name, total_points:Faker::Number.number(3)})
+  team = Team.create!({name:Faker::Name.name, total_points:Faker::Number.number(3), user:user1})
   12.times do
     Player.create!({name:Faker::Name.name, position:'PG', nba_team:Faker::Team.name, points:Faker::Number.number(2), team_id: team.id})
   end
