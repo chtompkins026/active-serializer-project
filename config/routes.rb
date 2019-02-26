d4c80c903d3298e2791fa1542eff249d1901afc4
chtompkins026 <chtompkins026@gmail.com>
2019-02-26 11:58:16 -0500

    add in omniauth initializer

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get '/signup' => 'users#new'
   get 'next_team/:id', to: "teams#next_team"
 end
