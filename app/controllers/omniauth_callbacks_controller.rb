284e5ca1b1fb12adabfbb20ea66b4df07b2c0b43
chtompkins026 <chtompkins026@gmail.com>
2019-02-26 11:00:26 -0500

    Adding in Omniauth functionality

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -21,13 +21,10 @@ class OmniauthCallbacksController < ApplicationController
       "(provider = ? AND uid = ?) OR email = ?",
       auth.provider, auth.uid, auth.info.email
     ).first_or_initialize.tap do |user|
-      #puts "USER: #{auth.inspect}"
       user.email = auth.info.email
       user.uid = auth.uid
       user.provider = auth.provider
-      # user.avatar_url = auth.info.image
-      # user.username = auth.info.name
-      # user.oauth_token = auth.credentials.token
+
       user.save!
     end
   end
