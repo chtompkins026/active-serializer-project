e6aada74eebc1d331dfbf8c502acb8fac63a05e2
chtompkins026 <chtompkins026@gmail.com>
2019-02-26 20:04:46 -0500

    updated the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,50 @@
 class CommentsController < ApplicationController
-
-  def show
-    @comment = Team.find(params[:id]).comments
-  end
+  before_action :set_comment, only: [:show, :edit, :update]
 
   def index
-    @comment = Comment.all
-    # @team = Team.find(params[:team_id])
-    # @league_id = League.find(params[:league_id])
-    if session[:user_id]
-      @user = User.find(session[:user_id])
+    @comments = comment.all
+  end
+
+  def show
+    @comment = comment.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @comment}
     end
+  end
+
+  def new
+    @comment = comment.new
   end
 
   def create
-    @comment = Comment.create(player_params)
-    if @comment.save
-      respond_to do |f|
-        f.json{
-          render json: @comment
-        }
-      end
-    else
-      render json: "#{@comment.errors.full_messages.join(", ")}"
-    end
+    @comment = comment.create(comment_params)
+    render json: @comment, status: 201
+  end
+
+  def edit
+  end
+
+  def update
+    @comment.update(comment_params)
+    redirect_to comment_path(@comment)
+  end
+
+  def comment_data
+    comment = comment.find(params[:id])
+    #render json: commentSerializer.serialize(comment)
+    render json: comment.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}])
   end
 
 private
-
-  def comment_params
-    params.require(:comment).permit(:content, :user_id, :team_id)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = comment.find(params[:id])
   end
 
-
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def comment_params
+    params.require(:comment).permit(:title, :description)
+  end
 end
