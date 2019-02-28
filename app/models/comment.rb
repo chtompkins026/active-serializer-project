253061c33d16e9983c5e724cec627aa29dd467b5
chtompkins026 <chtompkins026@gmail.com>
2019-02-28 10:28:48 -0500

    Added in scope methods to both comment and player model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
+
+  scope :sort_by_comment, -> {order("id DESC")}
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,6 @@ class Player < ApplicationRecord
   # belongs_to :team, optional: true
   has_many :player_teams
   has_many :teams, through: :player_teams
+
+  scope :sort_by_player, -> {order("points DESC")}
 end
