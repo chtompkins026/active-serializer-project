c6ea7359ea2fe6b435e3fbd30b8ddc26b3d26fb5
chtompkins026 <chtompkins026@gmail.com>
2019-02-28 14:14:53 -0500

    working on bug with comments show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,18 +44,16 @@ class CommentsController < ApplicationController
 
   def comment_data
     comment = Comment.find(params[:id])
-    #render json: commentSerializer.serialize(comment)
+
     render json: comment.to_json(only: [:title, :description, :id],
                               include: [user: { only: [:name]}])
   end
 
 private
-  # Use callbacks to share common setup or constraints between actions.
   def set_comment
     @comment = Comment.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
     params.require(:comment).permit(:title, :description)
   end
